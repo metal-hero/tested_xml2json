45beed0cf90204b979bd886cea766e5456dbdd14
Roman Khabirov <khabirovroman@gmail.com>
2015-03-12 18:06:10 +0600

    Finished testing

--- test/test_xml2json.py
+++ test/test_xml2json.py
@@ -43,5 +43,70 @@ class SimplisticTest(unittest.TestCase):
         self.assertTrue("table" in json_data["root"])
         self.assertEqual(json_data["root"]["table"][0]["tr"]["td"] , ["Apples", "Bananas"])
 
+    # def test_internal_to_elem(self):
+    #     dic = {'id':'15','name':'Vasya','is_clone':True}
+    #     f = xml2json.internal_to_elem(dic)
+    #     self.assertTrue(f is not None)
+
+    def test_json2xml(self):
+        u_dic = "{}"
+        string_dic = "{'#text':'Good day!','#tail':'Good bay!'}"
+        js_big = {'students': [{'name': 'Sula'}, {'name': 'Bekzat'}], 'surname': 'Khabirov', 'name': 'Roman'}
+        js = {'name':'Roman'}
+        js_err = "{'name':'Roman','surname':'Khabirov'}"
+        js2 = {'@sdsfd':'Roman'}
+        js3 = {'#text':'Good day!','#tail':'Good bay!'}
+        # res_big_false = xml2json.json2xml(js_big)
+        res_big_true = xml2json.json2xml({'root': js_big})
+        res = xml2json.json2xml(js)
+        res2 = xml2json.json2xml({'@root':js2})
+        res3 = xml2json.json2xml({'root':js3})
+        res4 = xml2json.json2xml({'root':js3})
+        # res_dic = xml2json.json2xml("{'root':"+string_dic+"}")
+
+        res_u = xml2json.json2xml(u_dic)
+        
+        # print res_big_true
+        # print str(res)
+        # print f(js) == 'AD'
+        # self.assertFalse(res_big_false == '<name>Roman</name><surname>Khabirov</surname><students><name>Sula</name><name>Bekzat</name></students>')
+        self.assertTrue(res == '<name>Roman</name>')
+        self.assertFalse(res2 == '<name>Roman</name>')
+        self.assertFalse(res3 == '<name>Roman</name>')
+        self.assertFalse(res4 == '<name>Roman</name>')
+        # self.assertFalse(res_dic == '<name>Roman</name>')
+        self.assertFalse(res_u == '<></>') 
+        res_err = xml2json.json2xml(js_err)
+        self.assertFalse(res_err == 'ValidationError!') 
+
+        self.assertTrue(res_big_true == '<root><students><name>Sula</name></students><students><name>Bekzat</name></students><surname>Khabirov</surname><name>Roman</name></root>')
+
+    def test_main(self):
+        self.assertFalse(xml2json.main() == None)
+
+    def test_xml2json(self):
+        class Bla():
+            pretty = True
+        bl = Bla()
+        xml_big = '<root><students><name>Sula</name></students><students><name>Bekzat</name></students><surname>Khabirov</surname><name>Roman</name></root>'
+        xml = '<name>Roman</name>'
+        # res_big_false = xml2json.json2xml(js_big)
+        res_big_true = xml2json.xml2json(xml_big,bl)
+        res = xml2json.xml2json(xml,bl)
+        # print res_big_true
+        # print str(res)
+        # print f(js) == 'AD'
+        # self.assertFalse(res_big_false == '<name>Roman</name><surname>Khabirov</surname><students><name>Sula</name><name>Bekzat</name></students>')
+        self.assertFalse(res == "{'name':'Roman'}")
+        self.assertFalse(res_big_true == "{'students': [{'name': 'Sula'}, {'name': 'Bekzat'}], 'surname': 'Khabirov', 'name': 'Roman'}")
+
+    def test_json2elem(self):
+        js = {}
+        # js['name'] = 'Roma'
+        # res = xml2json.json2elem(js)
+        # self.assertFalse(res == "{'name':'Roman'}")
+
+
+
 if __name__ == '__main__':
     unittest.main()
